55869ee0e0a0106d8e94821642a864fa226a7dea
Sabrina Mokerji <mokerjis@gmail.com>
2019-12-20 14:07:10 -0500

    Removes scoreColors since graph will no longer be changing color

--- src/app/components/CreditScoreSimulator.jsx
+++ src/app/components/CreditScoreSimulator.jsx
@@ -35,12 +35,20 @@ const useStyles = createUseStyles(theme => ({
   creditGauge: {
     position: 'absolute',
     width   : '100%',
+    overflow: 'hidden',
   },
   circle: {
     width   : '251px',
     position: 'relative',
     margin  : '0 auto',
     height  : '127px',
+  },
+  ticker: {
+    width       : '28px',
+    height      : '28px',
+    borderRadius: '50%',
+    background  : theme.color.white,
+    border      : `7px solid ${theme.color.green}`,
   },
 }))
 
@@ -51,10 +59,6 @@ export const CreditScoreSimulator = (props) => {
   const creditScore = value;
   const minScore    = 300;
   const maxScore    = 850;
-  const scoreColors = {
-    text   : theme.color.text,
-    default: theme.color.green,
-  };
 
   // Since the graph does not start at 0, get the range of numbers that the graph actually displays
   const graphRange = maxScore - minScore;
@@ -64,14 +68,6 @@ export const CreditScoreSimulator = (props) => {
   const graphPercentage = scoreWithinRange / graphRange;
   // Get the degrees to rotate by multiplying the percentage by 180 degrees
   const graphScore = graphPercentage * 180;
-
-  const tickerStyle = {
-    width       : '28px',
-    height      : '28px',
-    borderRadius: '50%',
-    background  : theme.color.white,
-    border      : `7px solid ${scoreColors.default}`,
-  };
 
   const tickerPosition = {
     transition: '.8s',
@@ -90,20 +86,20 @@ export const CreditScoreSimulator = (props) => {
             version   = "1.1"
           >
             <path
-              fill = {scoreColors.default}
+              fill = {theme.color.green}
               d    = "M18.0390265,126.914469 L5,126.893901 C5,56.8688388 61.139214,0 130.5,0 C199.755853,0 256,56.7627402 256,126.893901 L242.960451,126.893901 C241.36379,64.2646924 191.624758,14 130.5,14 C69.3685512,14 19.6253207,64.2756977 18.0390268,126.914459 Z"
             />
-            <text fill={scoreColors.text}>
+            <text fill={theme.color.text}>
               <tspan x="0" y="146">
                 {minScore}
               </tspan>
             </text>
-            <text fill={scoreColors.text}>
+            <text fill={theme.color.text}>
               <tspan x="238" y="146">
                 {maxScore}
               </tspan>
             </text>
-            <text className={classes.largeScore} fill={scoreColors.text}>
+            <text className={classes.largeScore} fill={theme.color.text}>
               <tspan x="79" y="124">
                 {creditScore}
               </tspan>
@@ -112,7 +108,7 @@ export const CreditScoreSimulator = (props) => {
           <div className={classes.circle}>
             <span className={classes.innerCircle}>&nbsp;</span>
             <div style={tickerPosition} className={classes.tickerWrapper}>
-              <div style={tickerStyle}>&nbsp;</div>
+              <div className={classes.ticker}>&nbsp;</div>
             </div>
           </div>
         </div>
